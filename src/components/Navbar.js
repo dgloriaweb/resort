e50537afa896ef4f8b947a558258c2d5ad830cf0
unknown <dgloria.web@gmail.com>
2020-04-14 00:28:15 +0100

    49 min added more navbar stuff

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../images/logo.svg'
+import { FaAlignRight } from 'react-icons/fa'
 
 export default class Navbar extends Component {
-    render() {
-        return (
-            <div>
-                <ul>
-                    <li><a href='/'>Home</a></li> 
-                    <li><a href='/rooms'>Rooms</a></li>
-                </ul>
-            </div>
-        )
-    }
+  state = {
+    isOpen: false,
+  }
+  handleToggle = (e) => {
+    this.setState({ isOpen: !this.state.isOpen })
+  }
+
+  render() {
+    return (
+      <nav className='navbar'>
+        <div className='nav-center'>
+          <div className='nav-header'>
+            <ul
+              className={this.state.isOpen ? 'nav-links show-nav' : 'nav-links'}
+            >
+              <li>
+                <Link to='/'>
+                  <img src={logo} />
+                </Link>
+              </li>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/rooms'>Rooms</Link>
+              </li>
+            </ul>
+            <button type='button' className='nav-btn'>
+              <FaAlignRight className='nav-icon' onClick={this.handleToggle} />
+            </button>
+          </div>
+        </div>
+      </nav>
+    )
+  }
 }
