0762eb188a2ce22a2daf73646be38be0fa104c76
unknown <dgloria.web@gmail.com>
2020-04-15 07:49:39 +0100

    toggle sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,25 +16,27 @@ export default class Navbar extends Component {
       <nav className='navbar'>
         <div className='nav-center'>
           <div className='nav-header'>
-            <ul
-              className={this.state.isOpen ? 'nav-links show-nav' : 'nav-links'}
+            <Link to='/'>
+              <img src={logo} alt='Beach Resort' />
+            </Link>
+            <button
+              type='button'
+              className='nav-btn'
+              onClick={this.handleToggle}
             >
-              <li>
-                <Link to='/'>
-                  <img src={logo} />
-                </Link>
-              </li>
-              <li>
-                <Link to='/'>Home</Link>
-              </li>
-              <li>
-                <Link to='/rooms'>Rooms</Link>
-              </li>
-            </ul>
-            <button type='button' className='nav-btn'>
-              <FaAlignRight className='nav-icon' onClick={this.handleToggle} />
+              <FaAlignRight className='nav-icon' />
             </button>
           </div>
+          <ul
+            className={this.state.isOpen ? 'nav-links show-nav' : 'nav-links'}
+          >
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/rooms'>Rooms</Link>
+            </li>
+          </ul>
         </div>
       </nav>
     )
