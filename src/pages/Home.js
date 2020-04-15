50350aa5a12b84a7256b6297348577a535e89755
unknown <dgloria.web@gmail.com>
2020-04-15 10:41:35 +0100

    71 mins continue adding banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,19 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
-  return <div>Hello from Home</div>
+  return (
+    <Hero>
+      <Banner
+        title='Welcome'
+        subtitle='to the Friends of Challenging Behaviour Counselling Services'
+      >
+        <Link to='/rooms' className='btn-primary'>
+          Rooms
+        </Link>
+      </Banner>
+    </Hero>
+  )
 }
